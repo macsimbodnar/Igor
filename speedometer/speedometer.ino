7019fcb550d26a8dfc01012b5e57694a60a22168
Max <max@keyless.io>
2021-01-24 18:03:58 +0100

    Proper speedometer

--- speedometer/speedometer.ino
+++ speedometer/speedometer.ino
@@ -27,31 +27,27 @@
 #define MOTOR_PIN3      6
 #define MOTOR_PIN4      7
 
-#define SENSOR_PIN      A0
-#define AVERAGE_FACTOR  0         // коэффициент сглаживания показаний (0 = не сглаживать)
-
-#define POLLING_RATE    500       // Milliseconds 
+#define SENSOR_PIN      2
 
 #define STEPS          (315*3)    // Standard X25.168 range 315 degrees at 1/3 degree steps
 
 #define MOTOR_ZERO      20
 #define MOTOR_MAX       255
-#define SENSOR_ZERO     0
-#define SENSOR_MAX      380
 
-// MOTOR 50% THRASHOLD
-#define MOTOR_HALF_PERCENT_TH   165
-#define SENSOR_HALF             ((SENSOR_MAX - SENSOR_ZERO) / 2)
+#define UPDATE_SLEEP_MS 1000
+#define CONST_CONV_CM_PER_MS_TO_KM_PER_H 36
+
+// 6 impulses -> full turn (360) so we have 39 cm per impulse
+#define CM_FOR_IMPULSE  39.0f  // [cm/impulse]
 
 /**
    GLOBALS
 */
 // For motors connected to digital pins 4,5,6,7
 SwitecX25 motor(STEPS, MOTOR_PIN1, MOTOR_PIN2, MOTOR_PIN3, MOTOR_PIN4);
-unsigned long last_check  = 0;
-int motor_position        = 0;
-int last_sensor_val       = 0;
 
+volatile unsigned int impulse_counter = 0;
+unsigned long last_check_ms = 0;
 
 /**
    SETUP
@@ -66,78 +62,102 @@ void setup(void) {
     // Set the motor to the defined zero
     motor.setPosition(MOTOR_ZERO);
 
-    // Init last_check
-    last_check = millis();
+    last_check_ms = millis();
+
+    // Setup interrupt
+    pinMode(SENSOR_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(SENSOR_PIN), interrupt_handler, FALLING);
 
 #ifdef LOGS
-    Serial.println("GO!");
+    Serial.println("Setup done...");
 #endif
 }
 
 
-/**
-   MAIN LOOP
-*/
-void loop(void) {
-    // Read past time
-    unsigned long now = millis();
+void loop() {
 
-    // Check sensors and update the position only after polling rate expires
-    if (now - last_check > POLLING_RATE) {
+    const unsigned int current_impulses = impulse_counter;
+    impulse_counter = 0;
 
-        // Read sensor value
-        int val = analogRead(SENSOR_PIN);
+    const float distance_from_last_check = current_impulses * CM_FOR_IMPULSE;
+    
+    const unsigned long now = millis();
+    const unsigned long elapsed_time_ms = now - last_check_ms;
+    last_check_ms = now;
 
-        if (val >= SENSOR_ZERO && val <= SENSOR_MAX) {
-            // Invert the value
-            val = SENSOR_MAX - val;
-
-            // Update only if the values differs
-            if (abs(val - last_sensor_val) > AVERAGE_FACTOR) {
-                last_sensor_val = val;
-
-                // Using different mapping if the sensor is under or above the 50%
-                if (last_sensor_val > SENSOR_HALF) {
-                    // We are over the 50%
-                    motor_position = map(last_sensor_val, SENSOR_HALF, SENSOR_MAX, MOTOR_HALF_PERCENT_TH + 1,
-                                         MOTOR_MAX);
-                } else {
-                    // We are under 50%
-                    motor_position = map(last_sensor_val, SENSOR_ZERO, SENSOR_HALF, MOTOR_ZERO, MOTOR_HALF_PERCENT_TH);
-                }
-
-                // Check if the motor position is in the range
-                if (motor_position <= MOTOR_MAX && motor_position >= MOTOR_ZERO) {
-                    // Set new position
-                    motor.setPosition(motor_position);
-                }
-
-#ifdef LOGS
-                else {
-                    Serial.println("Motor out of range!");
-                }
-
-#endif
-            }
-        }
-
-#ifdef LOGS
-        else {
-            Serial.println("Sensor out of range!");
-        }
-
-#endif
-
-#ifdef LOGS
-        char buff[50];
-        sprintf(buff, "RAW|AVERAGE SENSOR: %04d|%04d  ->  MOTOR: %04d", val, last_sensor_val,
-                motor_position);
-        Serial.println(buff);
-#endif
-
-        last_check = now;
+    const float speed_cm_per_ms = distance_from_last_check / (float)elapsed_time_ms;
+    const unsigned long km_per_sec = (unsigned long)(speed_cm_per_ms * CONST_CONV_CM_PER_MS_TO_KM_PER_H);
+    
+    const unsigned long motor_pos = calc_motor_position(km_per_sec);
+    
+    // Check if the motor position is in the range
+    if (motor_pos <= MOTOR_MAX && motor_pos >= MOTOR_ZERO) {
+        // Set new position
+        motor.setPosition(motor_pos);
     }
 
-    // the motor only moves when you call update
+#ifdef LOGS
+    Serial.print("imp: ");
+    Serial.print(current_impulses);
+    Serial.print(", dist cm: ");
+    Serial.print(distance_from_last_check);
+    Serial.print(", elapsed ms: ");
+    Serial.print(elapsed_time_ms);   
+    Serial.print(", speed cm/ms: ");
+    Serial.print(speed_cm_per_ms);
+    Serial.print(", Km/h: ");
+    Serial.print(km_per_sec);
+    Serial.print(", motor pos: ");
+    Serial.print(motor_pos);
+    Serial.print("\n");
+#endif
+
     motor.update();
+    delay(UPDATE_SLEEP_MS); 
+}
+
+
+void interrupt_handler() {
+    ++impulse_counter;
+}
+
+
+unsigned long calc_motor_position(const unsigned long km_h) {
+    int motor_position = 0;
+
+    if (km_h < 10) {
+        motor_position = map(km_h, 0, 9, MOTOR_ZERO, 65);
+    } else if (km_h < 20) {
+        motor_position = map(km_h, 10, 19, 0, 65);
+    } else if (km_h < 30) {
+        motor_position = map(km_h, 20, 29, 0, 65);
+    } else if (km_h < 40) {
+        motor_position = map(km_h, 30, 39, 0, 65);
+    } else if (km_h < 50) {
+        motor_position = map(km_h, 40, 49, 0, 65);
+    } else if (km_h < 60) {
+        motor_position = map(km_h, 50, 59, 0, 65);
+    } else if (km_h < 70) {
+        motor_position = map(km_h, 60, 69, 0, 65);
+    } else if (km_h < 80) {
+        motor_position = map(km_h, 70, 79, 0, 65);
+    } else if (km_h < 90) {
+        motor_position = map(km_h, 80, 89, 0, 65);
+    } else if (km_h < 100) {
+        motor_position = map(km_h, 90, 99, 0, 65);
+    } else if (km_h < 110) {
+        motor_position = map(km_h, 100, 109, 0, 65);
+    } else if (km_h < 120) {
+        motor_position = map(km_h, 110, 119, 0, 65);
+    } else if (km_h < 130) {
+        motor_position = map(km_h, 120, 129, 0, 65);
+    } else if (km_h < 140) {
+        motor_position = map(km_h, 130, 139, 0, 65);
+    } else if (km_h < 150) {
+        motor_position = map(km_h, 140, 149, 0, 65);
+    } else {
+        motor_position = map(km_h, 150, 500, 0, 65);
+    }
+
+    return motor_position;
 }
